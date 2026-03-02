e1cc1a22f74f1562ba89836d51937528b2ef098d
Ankit <knjangir24@gmail.com>
2026-03-03 00:44:39 +0530

    Update install script to set Prisma target dir env vars

--- install_prisma_render.sh
+++ install_prisma_render.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 pip install -r requirements.txt
+export PRISMA_CLI_QUERY_ENGINE_TYPE=binary
+export PRISMA_CLIENT_ENGINE_TYPE=binary
+export PRISMA_TARGET_DIR=/opt/render/project/src/
+export PRISMA_EXPECTED_ENGINE_VERSION=393aa359c9ad4a4bb28630fb5613f9c281cde053
 prisma py fetch
 prisma generate
